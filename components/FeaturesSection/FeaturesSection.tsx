32bc8b0c24ae4b58cf0e3d5ba2081e031e3ee7ed
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-07-10 23:48:03 +0330

    developed half of the features section

---
+++ components/FeaturesSection/FeaturesSection.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import React from 'react';
+import viewImage from '../../assets/images/beatifulView.png'
+import location from '../../assets/icons/locationTwo.svg'
+
+const FeaturesSection = () => {
+    return (
+        <div className='FeaturesSection w-full flex px-20 my-10'>
+            <div className='FeaturesSection__leftSection flex flex-col flex-1 gap-y-5'>
+                <h2
+                    className='text-4xl font-bold w-8/12'
+                    style={{ color: '#0C111F', lineHeight: '180%' }}
+                > Get Experience Which are fun </h2>
+                <p
+                    className='font-normal'
+                    style={{ color: '#0C111F', opacity: '0.6000000238418579' }}
+                >
+                    With DiscoverHub you can get the best experience on holiday travel, and we always update the latest and best destinations in the world
+                </p>
+                <div className='relative'>
+                    <div className='rounded-full absolute top-3.5 left-3.5 text-white px-3 py-3 text-sm flex items-center gap-x-1'
+                        style={{ background: 'rgba(255, 255, 255, 0.22)', backdropFilter: 'blur(7.5px)' }}
+                    >
+                        <Image src={location} alt='location' />
+                        Mazandaran
+                    </div>
+                    <Image src={viewImage} alt='view' />
+                </div>
+            </div>
+
+            <div className='FeaturesSection__rightSection'
+                style={{ flex: '1.5' }}
+            >
+
+            </div>
+        </div>
+    );
+};
+
+export default FeaturesSection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header/Header'
 import Introduction from '@/components/Introduction/Introduction'
+import FeaturesSection from '@/components/FeaturesSection/FeaturesSection'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,10 +11,11 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
     >
-      <div className='HeroSection w-full pt-5 px-20' style={{ background: '#0C111F' , fontFamily : 'Inter' }}>
+      <div className='HeroSection w-full pt-5 px-20' style={{ background: '#0C111F', fontFamily: 'Inter' }}>
         <Header />
         <Introduction />
       </div>
+      <FeaturesSection />
     </main>
   )
 }

1026b33cc8386128ace527cce5f4e85e1e4f08bb
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-08-18 01:45:04 +0330

    developed parts of leaflet routing machine

--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -7,6 +7,12 @@ import location from '../../assets/icons/location2.svg'
 import pic from '../../assets/images/place.jpg'
 import { createClient } from 'pexels';
 const Map = dynamic(() => import('../../components/Map'), { ssr: false });
+const Routing = dynamic(() => import("../../components/Map/Routing"), { ssr: false });
+import "leaflet/dist/leaflet.css";
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Paper from '@mui/material/Paper';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,95 +33,107 @@ interface Venue {
 }
 
 export default function MapContainer() {
+  // const classes = useStyles();
   const [userLocation, setUserLocation] = useState<UserLocation | null>(null);
   const [data, setData] = useState<Venue[]>([]);
-  const [pics,setPics] = useState<any>()
+  const [pics, setPics] = useState<any>()
+  const [selectedCity, setSelectedCity] = useState({});
 
   const client = createClient('XZYowhK5D5FH0PTSgEWfcbWGtK2v1gLVuROLcUowriW39LIhI1jV3qa2');
   const query = 'locations';
+
+  const handleLocationSelect = (latitude: number, longitude: number) => {
+    setSelectedCity({ latitude, longitude });
+  };
 
   useEffect(() => {
     client.photos.search({ query, per_page: 10 }).then(photos => {
       setPics(photos)
     });
+    const fetchData = async () => {
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'fsq3aCgzAkodfbe6Y0mhTbwzAJVr/U+Ls6sJJjujRz/EMOs='
+        }
+      };
+      try {
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            const { latitude, longitude } = position.coords;
+            setUserLocation({ latitude, longitude });
 
-  const fetchData = async () => {
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'fsq3aCgzAkodfbe6Y0mhTbwzAJVr/U+Ls6sJJjujRz/EMOs='
+            fetch(`https://api.foursquare.com/v3/places/nearby?ll=${latitude},${longitude}`, options)
+              .then(response => response.json())
+              .then(responseData => {
+                const venues: Venue[] = responseData.results || [];
+                setData(venues);
+              })
+              .catch(error => {
+                console.error('Error fetching data:', error);
+              });
+          },
+          error => {
+            console.error('Error getting user location:', error);
+          }
+        );
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
     };
-    try {
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          const { latitude, longitude } = position.coords;
-          setUserLocation({ latitude, longitude });
 
-          fetch(`https://api.foursquare.com/v3/places/nearby?ll=${latitude},${longitude}`, options)
-            .then(response => response.json())
-            .then(responseData => {
-              const venues: Venue[] = responseData.results || [];
-              setData(venues);
-            })
-            .catch(error => {
-              console.error('Error fetching data:', error);
-            });
-        },
-        error => {
-          console.error('Error getting user location:', error);
-        }
-      );
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
+    fetchData();
+  }, []);
 
-  fetchData();
-}, []);
-
-return (
-  <Map className='' width="800" height="400" center={userLocation ? [userLocation.latitude, userLocation.longitude] : [38.907132, -77.036546]} zoom={30}>
-    {({ TileLayer, Marker, Popup }) => (
-      <>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-        />
-        {userLocation && (
-          <Marker position={[userLocation.latitude, userLocation.longitude]}>
-            <Popup>
-              Your location
-            </Popup>
-          </Marker>
-        )}
-        {data.map((item,index) => (
-          <Marker width={200} key={item.fsq_id} position={[item.geocodes.main.latitude, item.geocodes.main.longitude]}>
-            <Popup>
-              <Image src={pic} alt='pic' />
-              <div className='px-3'>
-                <div className='flex items-center'>
-                  <div className='flex flex-col' style={{ margin: '10px 0px' }}>
-                    <p className={`text-base font-medium ${inter.className}`} style={{ margin: '0px 0px' }}>
-                      {item.name}
-                    </p>
-                    <p className='text-xs' style={{ margin: '0px 0px', color: 'gray' }}>
-                      {item.categories[0]?.name}
-                    </p>
-                  </div>
-                  <hr style={{ width: '34px', borderColor: 'lightgray', transform: 'rotate(90deg)' }} />
-                  <div className='flex flex-col items-center gap-1'>
-                    <Image src={location} alt='location' style={{ width: '20px' }} />
-                    <p style={{ margin: '0', color: 'gray' }} className='text-xs'> {item?.distance} mil </p>
+  return (
+    <Map className='' width="800" height="400" center={userLocation ? [userLocation.latitude, userLocation.longitude] : [38.907132, -77.036546]} zoom={30}>
+      {({ TileLayer, Marker, Popup }) => (
+        <>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          />
+          <Routing sourceCity={userLocation} destinationCity={selectedCity} />
+          {userLocation && (
+            <Marker position={[userLocation.latitude, userLocation.longitude]}>
+              <Popup>
+                Your location
+              </Popup>
+            </Marker>
+          )}
+          {data.map((item, index) =>
+          (
+            <Marker width={200} key={item.fsq_id} position={[item.geocodes.main.latitude, item.geocodes.main.longitude]}>
+              <Popup>
+                <Image src={pic} alt='pic' />
+                <div
+                  className='px-3'
+                  onClick={() => {
+                    handleLocationSelect(item.geocodes.main.latitude, item.geocodes.main.longitude);
+                  }}
+                >
+                  <div className='flex items-center'>
+                    <div className='flex flex-col' style={{ margin: '10px 0px' }}>
+                      <p className={`text-base font-medium ${inter.className}`} style={{ margin: '0px 0px' }}>
+                        {item.name}
+                      </p>
+                      <p className='text-xs' style={{ margin: '0px 0px', color: 'gray' }}>
+                        {item.categories[0]?.name}
+                      </p>
+                    </div>
+                    <hr style={{ width: '34px', borderColor: 'lightgray', transform: 'rotate(90deg)' }} />
+                    <div className='flex flex-col items-center gap-1'>
+                      <Image src={location} alt='location' style={{ width: '20px' }} />
+                      <p style={{ margin: '0', color: 'gray' }} className='text-xs'> {item?.distance} mil </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Popup>
-          </Marker>
-        ))}
-      </>
-    )}
-  </Map>
-);
+              </Popup>
+            </Marker>
+          ))}
+        </>
+      )}
+    </Map>
+  );
 }
